99fda8c239d8036951f28b11324fe71cd898812f
paulojomatos <paulojomatos@gmail.com>
2018-08-31 10:46:31 -0300

    Register a UI extension

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,26 +1,41 @@
-alert("The scripts from the unicorn-greetings loadable feature have been loaded!");
+alert('The scripts from the unicorn-greetings loadable feature have been loaded!');
 
 function MainService() {
     this.serviceName = 'unicorn-greetings:MainService';
 }
 
 MainService.prototype = {
-    implements: ['start', 'ready', 'stop'],
+    implements: ['start', 'ready', 'stop', 'trigger'],
 
     register: function () {
         SYMPHONY.services.make(this.serviceName, this, this.implements, true, true);
     },
 
     start: function () {
-        alert('start!');
+        alert(this.serviceName + '=> start!');
     },
 
     ready: function () {
-        alert('ready!');
+        alert(this.serviceName + '=> ready!');
+        // subscribe to the UI service
+        this.uiService = SYMPHONY.services.subscribe('ui');
+        // register a greeting button
+        this.uiService.registerExtension('room', 'unicorn-greeting-room', this.serviceName, {label: 'Unicorn Greeting Button'});
     },
 
     stop: function () {
-        alert('stop!');
+        alert(this.serviceName + '=> stop!');
+        // unregister our greeting button
+        this.uiService.unregisterExtension('room', 'unicorn-greeting-room');
+    },
+
+    /**
+     * @description Once the UI button is clicked, this method will be fired
+     */
+    trigger: function (uiClass, id) {
+        if(id === 'unicorn-greeting-room') {
+            alert('Heey! :)');
+        }
     }
 }
 
